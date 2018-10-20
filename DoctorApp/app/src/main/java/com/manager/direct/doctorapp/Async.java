6204bda1a01653e52ad7d6412e492faa3525fe0c
Kanzeparov <kanzeparov@yandex.ru>
2018-10-20 20:03:37 +0300

    added app

---
+++ DoctorApp/app/src/main/java/com/manager/direct/doctorapp/Async.java
@@ -0,0 +1,51 @@
+package com.manager.direct.doctorapp;
+
+import android.os.AsyncTask;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class Async extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        try {
+            // specify the host, protocol, and port
+            HttpHost target = new HttpHost("10.4.230.35", 3000, "http");
+
+            // specify the get request
+            HttpGet getRequest = new HttpGet("/contract");
+
+            System.out.println("executing request to " + target);
+
+            HttpResponse httpResponse = httpclient.execute(target, getRequest);
+            HttpEntity entity = httpResponse.getEntity();
+
+            System.out.println("----------------------------------------");
+            System.out.println(httpResponse.getStatusLine());
+            Header[] headers = httpResponse.getAllHeaders();
+            for (int i = 0; i < headers.length; i++) {
+                System.out.println(headers[i]);
+            }
+            System.out.println("----------------------------------------");
+
+            if (entity != null) {
+                System.out.println(EntityUtils.toString(entity));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // When HttpClient instance is no longer needed,
+            // shut down the connection manager to ensure
+            // immediate deallocation of all system resources
+            httpclient.getConnectionManager().shutdown();
+        }
+        return null;
+    }
+}
---
+++ RecyclerClient/app/src/main/java/com/manager/direct/recyclerclient/MainActivity.java
@@ -0,0 +1,179 @@
+package com.manager.direct.recyclerclient;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<DataModel> data;
+    static View.OnClickListener myOnClickListener;
+    private static ArrayList<Integer> removedItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myOnClickListener = new MyOnClickListener(this);
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        data = new ArrayList<DataModel>();
+        for (int i = 0; i < MyData.nameArray.length; i++) {
+            data.add(new DataModel(
+                    MyData.nameArray[i],
+                    MyData.versionArray[i],
+                    MyData.id_[i],
+                    MyData.drawableArray[i]
+            ));
+        }
+
+        removedItems = new ArrayList<Integer>();
+
+        adapter = new CustomAdapter(data);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    private static class MyOnClickListener implements View.OnClickListener {
+
+        private final Context context;
+
+        private MyOnClickListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+            removeItem(v);
+        }
+
+        private void removeItem(View v) {
+            int selectedItemPosition = recyclerView.getChildPosition(v);
+            boolean stateRem = true;
+            RecyclerView.ViewHolder viewHolder
+                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
+            TextView textViewName
+                    = (TextView) viewHolder.itemView.findViewById(R.id.textViewName);
+            String selectedName = (String) textViewName.getText();
+            int selectedItemId = -1;
+            for (int i = 0; i < MyData.nameArray.length; i++) {
+                if (selectedName.equals(MyData.nameArray[i])) {
+                    selectedItemId = MyData.id_[i];
+                    if(MyData.state[i] == false) {
+                        stateRem = false;
+                        showToast(v,false);
+                        MyData.state[i] = true;
+                    } else if(MyData.state[i] == true) {
+                        showToast(v,true);
+                    }
+                }
+            }
+
+            if (stateRem) {
+                removedItems.add(selectedItemId);
+                data.remove(selectedItemPosition);
+                adapter.notifyItemRemoved(selectedItemPosition);
+            }
+        }
+
+        public void showToast(View v, Boolean b) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            View layout = inflater.inflate(R.layout.toast,
+                    (ViewGroup) v.findViewById(R.id.toast_layout_root));
+
+            ImageView image = (ImageView) layout.findViewById(R.id.image);
+            if(b == true) {
+                image.setImageResource(R.drawable.ok);
+            }
+
+            if(b == false) {
+                image.setImageResource(R.drawable.no);
+            }
+            TextView text = (TextView) layout.findViewById(R.id.text);
+          //  text.setText("Hello! This is a custom toast!");
+            Toast toast = new Toast(context);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.setDuration(Toast.LENGTH_LONG);
+            toast.setView(layout);
+            toast.show();
+        }
+
+    }
+
+
+    public void showToast(View v) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast,
+                (ViewGroup) v.findViewById(R.id.toast_layout_root));
+
+        ImageView image = (ImageView) layout.findViewById(R.id.image);
+        image.setImageResource(R.drawable.ok);
+        TextView text = (TextView) layout.findViewById(R.id.text);
+      //  text.setText("Hello! This is a custom toast!");
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.add_item) {
+           //check if any items to add
+            if (removedItems.size() != 0) {
+                addRemovedItemToList();
+            } else {
+                Toast.makeText(this, "Nothing to add", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return true;
+    }
+
+    private void addRemovedItemToList() {
+        int addItemAtListPosition = 3;
+        data.add(addItemAtListPosition, new DataModel(
+                MyData.nameArray[removedItems.get(0)],
+                MyData.versionArray[removedItems.get(0)],
+                MyData.id_[removedItems.get(0)],
+                MyData.drawableArray[removedItems.get(0)]
+        ));
+        adapter.notifyItemInserted(addItemAtListPosition);
+        removedItems.remove(0);
+    }
+}
